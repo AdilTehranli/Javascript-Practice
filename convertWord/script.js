18e6e14d833c6f1623d38a590677944b4dad8f60
Adil Tehranli <adiljt@code.edu.az>
2024-05-15 20:48:33 +0400

    Updated reverse word

---
+++ convertWord/script.js
@@ -0,0 +1,5 @@
+// function convertWord() {
+//     let inp = document.getElementById("inp").value
+//     let reverse= inp.split("").reverse().join("")
+//     document.getElementById("conv").innerHTML = reverse
+// }
