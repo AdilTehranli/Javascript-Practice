f9cc9d89fb9e2f21cd4c6e3f6e5431b2956f36f8
Adil Tehranli <adiljt@code.edu.az>
2024-05-17 13:28:41 +0400

    Updated calculate

---
+++ calculatorApp/script.js
@@ -0,0 +1,29 @@
+function buttonKal(x) {
+    displayInput.value+=x.innerText
+}
+function equal(){
+    let result = 0;
+    if(displayInput.value.includes("+")){
+    let deyer = displayInput.value.split('+')
+    for(let i =0;i<deyer.length;i++){
+        result+=(+deyer[i])
+    }
+    }else if(displayInput.value.includes("-")){
+        let deyer = displayInput.value.split('-')
+        result=(+deyer[0])
+        for(let i =1;i<deyer.length;i++){
+            result-=(+deyer[i])
+        }
+    }else if(displayInput.value.includes('/')){
+        let deyer = displayInput.value.split('/')
+      result=(+deyer[0])/(+deyer[1])
+    }else if(displayInput.value.includes('*')){
+        let deyer = displayInput.value.split('*')
+      result=(+deyer[0])*(+deyer[1])
+    }
+    displayInput.value=result
+}
+
+function sil(){
+    displayInput.value=" "
+}
