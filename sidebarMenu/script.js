433ee88bd1cf326aa088fd1b86696d6af88fb2ac
Adil Tehranli <adiljt@code.edu.az>
2024-05-21 13:43:05 +0400

    Updated sideBarD-none

--- sidebarMenu/script.js
+++ sidebarMenu/script.js
@@ -4,8 +4,16 @@ let menubar = document.querySelector(".menu-bar")
 let closeBar = document.querySelector(".menu-close").addEventListener("click",function(){
    menu.classList.remove("active")
    menubar.classList.remove("d-none")
+   document.querySelectorAll("ul li").forEach((x)=>{
+    x.children[1].classList.remove("d-none")
+    x.children[0].style.transform="scaleX(0)"
+   })
 })
 menubar.addEventListener("click",function (){
     menu.classList.add("active")
     menubar.classList.add("d-none")
+    document.querySelectorAll("ul li").forEach((x)=>{
+        x.children[1].classList.add("d-none")
+        x.children[0].style.transform="scaleX(1)"
+    })
 })
