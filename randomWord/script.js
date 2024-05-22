0e72d4679f7bd967f51cece945fb4c98c2dc19a7
Adil Tehranli <adiljt@code.edu.az>
2024-05-22 20:51:27 +0400

    created:randomWord

---
+++ randomWord/script.js
@@ -0,0 +1,18 @@
+let btn = document.querySelector(".card button")
+let card = document.querySelector(".card")
+
+
+let wordsH2= [" Never hit a man when he is down.","Two heads are better than one."," East or West, home is best"]
+let wordsP = ["Robert","Alex","Yulia"]
+
+function randomWordGenerator() {
+    const randomIndex = Math.floor(Math.random() * wordsH2.length)
+    const randomParag = Math.floor(Math.random() * wordsP.length)
+    const randomH2= wordsH2[randomIndex]
+    const randomP= wordsP[randomParag]
+    document.querySelector(".card h2").innerHTML = randomH2
+    document.querySelector(".card p").innerHTML = randomP
+}
+
+
+btn.addEventListener("click", randomWordGenerator)
