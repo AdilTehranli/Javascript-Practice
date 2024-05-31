2f7c542b89f8ca7038ac1b2c85f9e0f9d7c31948
Adil Tehranli <adiljt@code.edu.az>
2024-05-31 23:27:46 +0400

    Created

--- addStudent/script.js
+++ addStudent/script.js
@@ -1,61 +1,79 @@
-let users = [
-  {
-    name: "Kamil",
-    surname: "Memmedov",
-  },
-  {
-    name: "Arzu",
-    surname: "Memmedova",
-  },
-  {
-    name: "Aysel",
-    surname: "Karimova",
-  },
-  {
-    name: "Yulia",
-    surname: "Aleksandrovic",
-  },
-  {
-    name: "Katya",
-    surname: "Alexsa",
-  },
-  {
-    name: "Olqa",
-    surname: "Vurtasova",
-  },
-];
-let nameInp 
-let surnameInp
-let allTable = document.querySelector(".table tbody")
+// let users = [
+//   {
+//     name: "Kamil",
+//     surname: "Memmedov",
+//   },
+//   {
+//     name: "Arzu",
+//     surname: "Memmedova",
+//   },
+//   {
+//     name: "Aysel",
+//     surname: "Karimova",
+//   },
+//   {
+//     name: "Yulia",
+//     surname: "Aleksandrovic",
+//   },
+//   {
+//     name: "Katya",
+//     surname: "Alexsa",
+//   },
+//   {
+//     name: "Olqa",
+//     surname: "Vurtasova",
+//   },
+// ];
+// let nameInp 
+// let surnameInp
+// let allTable = document.querySelector(".table tbody")
 
-function sendData() {
-    const inpObj={};
+// function sendData() {
+//     const inpObj={};
     
-     nameInp = document.querySelector(".name")
-     surnameInp = document.querySelector(".surname")
+//      nameInp = document.querySelector(".name")
+//      surnameInp = document.querySelector(".surname")
 
-    inpObj.name = nameInp.value
-    inpObj.surname = surnameInp.value
-    users.push(inpObj)
-getData()
+//     inpObj.name = nameInp.value
+//     inpObj.surname = surnameInp.value
+//     users.push(inpObj)
+// getData()
 
-}
-getData()
+// }
+// getData()
 
-function getData() {
-    allTable.innerHTML=""; 
-      users.forEach((item,index)=>{
-        allTable.innerHTML+=`  <tr>
-        <th scope="row">${index+1}</th>
-        <td class="name">${item.name}</td>
-        <td class="surname" >${item.surname}</td>
-        <td  onclick="deleteRow(${index})" ><i class="fa-solid fa-trash"></i></td>
-        </tr>`
-    })
-}
+// function getData() {
+//     allTable.innerHTML=""; 
+//       users.forEach((item,index)=>{
+//         allTable.innerHTML+=`  <tr>
+//         <th scope="row">${index+1}</th>
+//         <td class="name">${item.name}</td>
+//         <td class="surname" >${item.surname}</td>
+//         <td  onclick="deleteRow(${index})" ><i class="fa-solid fa-trash"></i></td>
+//         </tr>`
+//     })
+// }
 
-function deleteRow(index) {
-    users.splice(index,1)
-    getData()
-}
+// function deleteRow(index) {
+//     users.splice(index,1)
+//     getData()
+// }
     
+
+const random={
+  name:"adil",
+  info(){
+    console.log("hello" + this.name)
+  }
+}
+random.info()
+
+// var value =50;
+
+// function results() {
+//     var value = 100;
+
+//     console.log("value" + value);
+//     console.log("value" + this.value);
+// }
+// results()
