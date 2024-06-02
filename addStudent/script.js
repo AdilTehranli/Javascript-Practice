872d1c97a8bf0d6ae9819d6e409d3c4a0c471759
Adil Tehranli <adiljt@code.edu.az>
2024-06-02 14:36:09 +0400

    Updated:AddStudent Edit-Method

--- addStudent/script.js
+++ addStudent/script.js
@@ -1,79 +1,113 @@
-// let users = [
-//   {
-//     name: "Kamil",
-//     surname: "Memmedov",
-//   },
-//   {
-//     name: "Arzu",
-//     surname: "Memmedova",
-//   },
-//   {
-//     name: "Aysel",
-//     surname: "Karimova",
-//   },
-//   {
-//     name: "Yulia",
-//     surname: "Aleksandrovic",
-//   },
-//   {
-//     name: "Katya",
-//     surname: "Alexsa",
-//   },
-//   {
-//     name: "Olqa",
-//     surname: "Vurtasova",
-//   },
-// ];
-// let nameInp 
-// let surnameInp
-// let allTable = document.querySelector(".table tbody")
+"use strict";
 
-// function sendData() {
-//     const inpObj={};
-    
-//      nameInp = document.querySelector(".name")
-//      surnameInp = document.querySelector(".surname")
+let users = [
+  {
+    name: "Kamil",
+    surname: "Memmedov",
+  },
+  {
+    name: "Arzu",
+    surname: "Memmedova",
+  },
+  {
+    name: "Aysel",
+    surname: "Karimova",
+  },
+  {
+    name: "Yulia",
+    surname: "Aleksandrovic",
+  },
+  {
+    name: "Katya",
+    surname: "Alexsa",
+  },
+  {
+    name: "Olqa",
+    surname: "Vurtasova",
+  },
+];
 
-//     inpObj.name = nameInp.value
-//     inpObj.surname = surnameInp.value
-//     users.push(inpObj)
-// getData()
+let nameInp;
+let surnameInp;
+let allTable = document.querySelector(".table tbody");
+let isEditing = false;
+let editingIndex = null;
 
-// }
-// getData()
-
-// function getData() {
-//     allTable.innerHTML=""; 
-//       users.forEach((item,index)=>{
-//         allTable.innerHTML+=`  <tr>
-//         <th scope="row">${index+1}</th>
-//         <td class="name">${item.name}</td>
-//         <td class="surname" >${item.surname}</td>
-//         <td  onclick="deleteRow(${index})" ><i class="fa-solid fa-trash"></i></td>
-//         </tr>`
-//     })
-// }
-
-// function deleteRow(index) {
-//     users.splice(index,1)
-//     getData()
-// }
-    
-
-const random={
-  name:"adil",
-  info(){
-    console.log("hello" + this.name)
+function handleButtonClick() {
+  if (isEditing) {
+    updateData();
+  } else {
+    sendData();
   }
 }
-random.info()
 
-// var value =50;
+function sendData() {
+  const inpObj = {};
 
-// function results() {
-//     var value = 100;
+  nameInp = document.querySelector(".name");
+  surnameInp = document.querySelector(".surname");
 
-//     console.log("value" + value);
-//     console.log("value" + this.value);
-// }
-// results()
+  inpObj.name = nameInp.value;
+  inpObj.surname = surnameInp.value;
+  users.push(inpObj);
+  getData();
+  clearInputs();
+}
+
+function getData() {
+  allTable.innerHTML = "";
+  users.forEach((item, index) => {
+    allTable.innerHTML += `  <tr>
+        <th scope="row">${index + 1}</th>
+        <td class="name">${item.name}</td>
+        <td class="surname" >${item.surname}</td>
+        <td  onclick="deleteRow(${index})" ><i class="fa-solid fa-trash"></i></td>
+        <td class="update" onclick="edit(${index})"  ><i class="fa-solid fa-pen-to-square"></i></td>
+        </tr>`;
+  });
+}
+
+function deleteRow(index) {
+  users.splice(index, 1);
+  getData();
+}
+
+function edit(index) {
+  isEditing = true;
+  editingIndex = index;
+
+  nameInp = document.querySelector(".name");
+  surnameInp = document.querySelector(".surname");
+
+  nameInp.value = users[index].name;
+  surnameInp.value = users[index].surname;
+
+  const button = document.querySelector("button[onclick='handleButtonClick()']");
+  button.innerText = "Update Row";
+}
+
+function updateData() {
+  if (editingIndex === null) {
+    return;
+  }
+  nameInp = document.querySelector(".name");
+  surnameInp = document.querySelector(".surname");
+
+  users[editingIndex].name = nameInp.value;
+  users[editingIndex].surname = surnameInp.value;
+
+  isEditing = false;
+  editingIndex = null;
+
+  const button = document.querySelector("button[onclick='handleButtonClick()']");
+  button.innerText = "Add Row";
+
+  getData();
+  clearInputs();
+}
+
+function clearInputs() {
+  document.querySelector(".name").value = "";
+  document.querySelector(".surname").value = "";
+}
+getData();
--- emailValidation/script.js
+++ emailValidation/script.js
@@ -1,14 +1,14 @@
-function validate() {
-    let mail=document.getElementById("text").value
+// function validate() {
+//     let mail=document.getElementById("text").value
 
-    let regx = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9]+).([a-z]+)(.[a-z]+)?$/
+//     let regx = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9]+).([a-z]+)(.[a-z]+)?$/
 
-    if(regx.test(mail)){
-        alert("Validate true")
-        return true
-    }
-    else{
-        alert("Validate false")
-        return false
-    }
-}
+//     if(regx.test(mail)){
+//         alert("Validate true")
+//         return true
+//     }
+//     else{
+//         alert("Validate false")
+//         return false
+//     }
+// }
